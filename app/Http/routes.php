6f8375a8c4cde3ef400da37934cea085166997c6
Walter Discher Cechinel <mistrim@gmail.com>
2015-07-23 23:25:57 +0000

    Fix update route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,4 +20,4 @@ Route::get('client', 'ClientController@index');
 Route::post('client', 'ClientController@store');
 Route::get('client/{id}', 'ClientController@show');
 Route::delete('client/{id}', 'ClientController@destroy');
-Route::patch('client/{id}', 'ClientController@update');
+Route::put('client/{id}', 'ClientController@update');
