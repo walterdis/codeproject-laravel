5602346bb28b54bf92ac710e7b09a3ac5df55885
Walter Discher Cechinel <mistrim@gmail.com>
2015-07-27 16:45:30 +0000

    Controller refactor

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,18 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var Repository
+     */
+    protected $repository;
+
+    /**
+     * @var Service
+     */
+    protected $service;
 }
